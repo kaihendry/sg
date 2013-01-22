410a53f8ada53c4677556c1527ca3d90551950d4
Kai Hendry <hendry@webconverger.com>
2013-01-22 11:06:31 +0000

    fix JSON producing script

--- all-json.sh
+++ all-json.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-# TODO: Bug with trailing comma
-echo '['
+o="${0%-*}.json"
+
+tmp=$(mktemp)
 cat *.csv | while read epoch y
 do
-	printf '{ "epoch" : "%s", "y" : %s }' "$epoch" "$y"
-	echo ,
+	printf '{ "epoch" : "%s", "y" : %s }\n' "$epoch" "$y" >> $tmp
 done
-echo ']'
+echo -n '[' > $o
+cat $tmp | tr '\n' ',' | sed -e 's/,$//' >> $o
+echo ']' >> $o
+
+rm -f $tmp
