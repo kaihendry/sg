1bd33db482692f88fb61be2db4a28af3d0fac0d9
Kai Hendry <hendry@webconverger.com>
2013-02-03 20:39:08 +0800

    re-factored morris

--- g/morris/morris.sh
+++ g/morris/morris.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-tmp=$(mktemp)
 cat *.csv | while read epoch y
 do
-	printf '{x: %s, y: %s }\n' "${epoch}000" "$y" >> $tmp
+	printf '{x: %s, y: %s },\n' "${epoch}000" "$y"
 done
-while read line; do echo "$line,"; done < $tmp
-rm -f $tmp
