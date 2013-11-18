80ac31e0b093832e9b207a53bb2e0efaf5888f5b
Kai Hendry <hendry@webconverger.com>
2013-11-18 14:34:50 +0800

    Tweaks to check for valid epoch and values

--- g/google/google.sh
+++ g/google/google.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-cat *.csv | while read epoch y
+cat *.csv | while read epoch val
 do
-	printf 'data.addRow([new Date(%s), %s]);\n' "${epoch}000" "$y"
+	test "$val" || continue
+	test "$epoch" -gt 100000 || continue
+	printf 'data.addRow([new Date(%s), %s]);\n' "${epoch}000" "$val"
 done
