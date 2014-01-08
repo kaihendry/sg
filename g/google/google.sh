b02f5604a990df86b665b412a657780b8a6496ee
Kai Hendry <hendry@webconverger.com>
2014-01-08 10:21:01 +0800

    Added silly filebased API to limit to last X days

--- g/google/google.sh
+++ g/google/google.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
+
+if test "$1" -gt 0 2>/dev/null
+then
+
+cat $(ls -t *.csv | head -n$1) | while read epoch val
+do
+	test "$val" || continue
+	test "$epoch" -gt 100000 || continue
+	printf 'data.addRow([new Date(%s), %s]);\n' "${epoch}000" "$val"
+done
+
+else
+
 cat *.csv | while read epoch val
 do
 	test "$val" || continue
 	test "$epoch" -gt 100000 || continue
 	printf 'data.addRow([new Date(%s), %s]);\n' "${epoch}000" "$val"
 done
+
+fi
