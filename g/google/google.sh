443b212d45cdfe246971d94206af9002599687fd
Kai Hendry <hendry@webconverger.com>
2014-09-09 18:14:46 +0800

    Too slow

--- g/google/google.sh
+++ g/google/google.sh
@@ -2,23 +2,33 @@
 
 if test "$1" -gt 0 2>/dev/null
 then
-
-cat $(ls -t *.csv | head -n$1) | while read epoch val _
-do
-	test "$val" || continue
-	# Make sure it's later than 3/3/1973
-	test "$epoch" -gt 100000000 || continue
-	printf 'data.addRow([new Date(%s), %s]);\n' "${epoch}000" "$val"
-done
-
+	top=$1
 else
-
-cat *.csv | while read epoch val _
-do
-	test "$val" || continue
-	# Make sure it's later than 3/3/1973
-	test "$epoch" -gt 100000000 || continue
-	printf 'data.addRow([new Date(%s), %s]);\n' "${epoch}000" "$val"
-done
-
+	# get all
+	top="-0"
 fi
+
+f=$(( $(awk 'NR==1 { print NF }' $(ls -t *.csv | head -n1)) - 1))
+echo Fields: $f 1>&2
+
+echo '["Date", "", ""],'
+
+cat $(ls -t *.csv | head -n$top) | while read -a fields
+do
+	# Make sure we have a reading
+	test "${fields[1]}" || continue
+	# Make sure it's later than 3/3/1973
+	test "${fields[0]}" -gt 100000000 || continue
+	echo -n "[new Date(${fields[0]}000)"
+	for i in $(seq 1 $f)
+	do
+		val=${fields[$i]}
+		if test "$val"
+		then
+			echo -n ", $val"
+		else
+			echo -n ", null"
+		fi
+	done
+	echo "],"
+done
--- g/morris/morris.sh
+++ g/morris/morris.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-cat *.csv | while read epoch y
+cat *.csv | while read epoch y _
 do
+	# Make sure it's later than 3/3/1973
+	test "$epoch" -gt 100000000 || continue
+	test "$y" || continue
 	printf '{x: %s, y: %s },\n' "${epoch}000" "$y"
 done
